e61157baa77d8f65cc8a067c7c30c0ab33601100
Jonathan Hooper <jonathan.hooper@gsa.gov>
2017-07-07 14:53:16 -0500

    Remove jekyll build artifacts

--- _site/sites/all/modules/custom/performance_api_client/js/simpleshowhide/index.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function(){
-	jQuery('.show-hide').hide();
-    jQuery('.toggle-text').css({cursor:'pointer'});
-	jQuery('.toggle-text').click(function(){
-		jQuery(this).toggleClass('toggle-text-expanded');
-		jQuery(this).next('.show-hide').slideToggle("medium");
-		return true;
-	});
-});
